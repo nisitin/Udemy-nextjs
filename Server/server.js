87fc33dbae4b697093f3ff5b9f5732414841af10
nisitin <seiya110307@icloud.com>
2021-02-02 21:00:59 +0900

    mongodbに接続可能

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 const app = express();
+
+//db接続する処理を書く
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    .then(() => console.log(`DB Connected`))
+    .catch((err) => console.log("DB connection error", err))
 
 app.get("/api/:message", (req, res) => {
     res.json({
